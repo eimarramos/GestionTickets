8ddd674d8ea9c882a359a7a14b2d988925ea8420
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-06-13 00:09:36 +0200

    Añadir inyección de dependencias y servicio de tickets
    Se ha creado el archivo `DependencyInjection.cs` para registrar el `TicketService` en el contenedor de inyección de dependencias.
    
    Además, se ha actualizado el archivo `GestionTickets.Application.csproj` para incluir la referencia al paquete `Microsoft.Extensions.DependencyInjection.Abstractions`.
    
    Por último, se ha implementado la clase `TicketService` con métodos asíncronos para gestionar tickets, incluyendo operaciones para obtener, agregar, actualizar y eliminar tickets, así como para obtener meses y años disponibles.

---
+++ src/GestionTickets.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using GestionTickets.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GestionTickets.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<TicketService>();
+
+        return services;
+    }
+}
---
+++ src/GestionTickets.Application/Services/TicketService.cs
@@ -0,0 +1,40 @@
+﻿using GestionTickets.Domain.Entities;
+using GestionTickets.Domain.Repositories;
+
+namespace GestionTickets.Application.Services
+{
+    public class TicketService
+    {
+        private readonly ITicketRepository _ticketRepository;
+
+        public TicketService(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public async Task<ICollection<Ticket>> GetTicketsByMonthAsync(int month, int year)
+        {
+            return await _ticketRepository.GetTicketsByMonthAsync(month, year);
+        }
+
+        public async Task AddTicketAsync(Ticket ticket)
+        {
+            await _ticketRepository.AddTicketAsync(ticket);
+        }
+
+        public async Task UpdateTicketAsync(Ticket ticket)
+        {
+            await _ticketRepository.UpdateTicketAsync(ticket);
+        }
+
+        public async Task DeleteTicketAsync(int id)
+        {
+            await _ticketRepository.DeleteTicketAsync(id);
+        }
+
+        public async Task<ICollection<Tuple<int, int>>> GetAvailableMonthsAndYearsAsync()
+        {
+            return await _ticketRepository.GetAvailableMonthsAndYearsAsync();
+        }
+    }
+}
